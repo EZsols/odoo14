6b7612c90bba362c280137e662a4f0ae8fc91740
pythoniclad <byte.yocto@gmail.com>
2021-08-23 03:25:29 +0500

    doc file updated again

--- ez_product_brands/__manifest__.py
+++ ez_product_brands/__manifest__.py
@@ -5,17 +5,17 @@
     "name": "Product Brands",
     "summary": 'Product Brands',
     'description': """
-       By using this app user can filtere results of products based on different brands. 
+       Using this app user can filter results of products based on different brands. 
        This app facilitates you to add new brands in your inventory, 
        assign them to respective products and easily filter, view products by their assigned brand.
     """,
     "sequence": 1,
     "category": "Inventory Management",
 
-    "author": "EZ SOlutions",
+    "author": "EZ Solutions",
     "website": "",
     "version": '14.0.0.1',
-    'price': 11.99,
+    'price': 9.99,
     'currency': 'USD',
     'license': 'OPL-1',
     "depends": ['base','stock'],
